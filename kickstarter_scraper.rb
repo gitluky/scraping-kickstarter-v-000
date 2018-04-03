2fef8d4cb0a356dff9e93a62e268d2d7b237a826
Jonathan Luk <jluk87@gmail.com>
2018-04-03 19:37:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,13 +9,20 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 
-#project: kickstarter.css('li.project.grid_4')
-#title: project.css("h2.bbcard_name strong a").text
-#image link: project.css('div.project-thumbnail a img').attribute('src').value
-#description: project.css('.bbcard_blurb').text
-#location: project.css('.project-meta li a').text
-#funded: project.css('.first.funded strong').text.to_i
+#projects: kickstarter.css('li.project.grid_4')
+#title: projects.css("h2.bbcard_name strong a").text
+#image link: projects.css('div.project-thumbnail a img').attribute('src').value
+#description: projects.css('.bbcard_blurb').text
+#location: projects.css('.project-meta li a').text
+#funded: projects.css('.first.funded strong').text.to_i
 
+  projects.each do |project_info|
+    project = Project.New
+    project.title = project_info.css("h2.bbcard_name strong a").text
+    project.img_link = project_info_info.css('div.project-thumbnail a img').attribute('src').value
+    project.description = project_info.css('.bbcard_blurb').text
+    project.location = project_info.css('.project-meta li a').text
+    project.funded = project_info.css('.first.funded strong').text.to_i
 end
 
 create_project_hash
