ce1932ee98b8ae058985393d0d60a8683efa6499
Jonathan Luk <jluk87@gmail.com>
2018-04-03 17:19:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,10 @@ def create_project_hash
 
   kickstarter = Nokogiri::HTML(html)
   binding.pry
+
+#project: kickstarter.css('li.project.grid_4')
+#title: project.css("h2.bbcard_name strong a").text
+
 end
 
 create_project_hash
