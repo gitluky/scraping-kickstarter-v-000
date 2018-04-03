c32547e1c9eaf0f61e040067af857795ddfcd8be
Jonathan Luk <jluk87@gmail.com>
2018-04-03 20:02:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,10 +18,10 @@ def create_project_hash
   kickstarter.css('li.project.grid_4').each.tap do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      :image_link = project.css('div.project-thumbnail a img').attribute('src').value
-      :description = project.css('.bbcard_blurb').text
-      :location = project.css('.project-meta span.location-name').text
-      :funded = project.css('.first.funded strong').text.gsub('%','').to_i
+      :image_link => project.css('div.project-thumbnail a img').attribute('src').value,
+      :description => project.css('.bbcard_blurb').text,
+      :location => project.css('.project-meta span.location-name').text,
+      :funded => project.css('.first.funded strong').text.gsub('%','').to_i
     }
   end
 end
